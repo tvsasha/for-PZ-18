80e9a2e2ba4480f2a803910520174065b90cc31e
Александр Туйгунов <tvsasha2006@gmail.com>
2024-02-15 18:00:05 +0500

    Добавьте файлы проекта.

---
+++ Therapeutic department/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Therapeutic_department
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
